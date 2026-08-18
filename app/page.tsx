0b8f3336c1555b779a1c54b233f3719ed76e399a
azijulhakimbd <azijulhakim.sherpur@gmail.com>
2026-08-18 23:22:23 +0600

    feat:home page all feature add

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 
 
 import Hero from "../components/home/hero";
+import AboutPage from "./about/page";
+import CertificationsPage from "./certifications/page";
+import ExperiencePage from "./experience/page";
+import ProjectsPage from "./projects/page";
+import SkillsPage from "./skills/page";
 
 export default function Home() {
   return (
     <main>
      <Hero />
+     <AboutPage />
+     <SkillsPage />
+     <ExperiencePage />
+     <ProjectsPage/>
+     <CertificationsPage />
     </main>
   );
 }
